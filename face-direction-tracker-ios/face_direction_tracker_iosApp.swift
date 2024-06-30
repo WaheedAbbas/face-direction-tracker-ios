80b9aa508c164017ad8f6385a3ce55ec9ec8fef7
Waheed Abbas <abbaxwaheed@gmail.com>
2024-06-30 20:29:30 +0500

    Initial commit

---
+++ face-direction-tracker-ios/face_direction_tracker_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  face_direction_tracker_iosApp.swift
+//  face-direction-tracker-ios
+//
+//  Created by Waheed Abbas on 30/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct face_direction_tracker_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
